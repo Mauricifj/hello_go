62553a40a4b251fbb6efca2ef985927bd34103e4
Maurici Ferreira Junior <31556059+Mauricifj@users.noreply.github.com>
2020-03-25 01:20:28 -0300

    Updates related to exploring functions

--- hello_world.go
+++ hello_world.go
@@ -1,28 +1,53 @@
 package main
-import "fmt"
+
+import (
+	"fmt"
+	"os"
+)
 
 func main() {
-	version := "1.0"
-	fmt.Println("*** Program HelloWorld - Version", version, "***")
+	welcome()
+
+	showMenu()
+
+	option := readOption()
+
+	switch option {
+	case 1:
+		fmt.Println("Hello, World")
+	case 2:
+		showCustomHelloWorld()
+	case 0:
+		fmt.Println("Process finishing...")
+		os.Exit(0)
+	default:
+		fmt.Println("Invalid option")
+		os.Exit(-1)
+	}
+}
+
+func showCustomHelloWorld() {
+	fmt.Print("Type your name, please: ")
+	var name string
+	fmt.Scan(&name)
+	fmt.Println("Hello,", name)
+}
+
+func showMenu() {
 	fmt.Println("--- MENU ---")
 	fmt.Println("1 - Show Hello World")
 	fmt.Println("2 - Show custom Hello World")
 	fmt.Println("0 - Quit")
+}
 
+func welcome() {
+	version := "1.0"
+	fmt.Println("*** Program HelloWorld - Version", version, "***")
+}
+
+func readOption() int {
 	fmt.Print("Choose one option, please: ")
-	option := -1
+	var option int
 	fmt.Scan(&option)
-	if option == 1 {
-		fmt.Println("Hello, World")
-	} else if option == 2 {
-		fmt.Print("Type your name, please: ")
-		var name string
-		fmt.Scan(&name)
-		fmt.Println("Hello,", name)
-	} else if option == 0 {
-		fmt.Println("Process finishing...")
-	} else {
-		fmt.Println("Invalid option")
-	}
-	fmt.Println("Process ended successfully")
+	return option
 }
